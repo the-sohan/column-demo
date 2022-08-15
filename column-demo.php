b09c675a4bf1337a969a270b07021129af7a2b17
Sohan Chowdhury <89298094+the-sohan@users.noreply.github.com>
2022-08-15 11:22:48 +0600

    Update column-demo.php

--- column-demo.php
+++ column-demo.php
@@ -190,5 +190,60 @@ function coldemo_thumbnail_filter_data( $wpquery ) {
 
 add_action( 'pre_get_posts', 'coldemo_thumbnail_filter_data' );
 // End Thumbnail Filter 
-// End Thumbnail Filter 
+
+// Start WordCount Filter
+function coldemo_wordcount_filter() {
+	if ( isset( $_GET['post_type'] ) && $_GET['post_type'] != 'post' ) { //display only on posts page
+		return;
+	}
+	$filter_value = isset( $_GET['THFILTER'] ) ? $_GET['THFILTER'] : '';
+	$values       = array(
+		'0' => __( 'Thumbnail Status', 'column_demo' ),
+		'1' => __( 'Has Thumbnail', 'column_demo' ),
+		'2' => __( 'No Thumbnail', 'column_demo' ),
+	);
+	?>
+    <select name="THFILTER">
+		<?php
+		foreach ( $values as $key => $value ) {
+			printf( "<option value='%s' %s>%s</option>", $key,
+				$key == $filter_value ? "selected = 'selected'" : '',
+				$value
+			);
+		}
+		?>
+    </select>
+	<?php
+}
+
+add_action( 'restrict_manage_posts', 'coldemo_wordcount_filter' );
+
+function coldemo_wordcount_filter_data( $wpquery ) {
+	if ( ! is_admin() ) {
+		return;
+	}
+
+	$filter_value = isset( $_GET['THFILTER'] ) ? $_GET['THFILTER'] : '';
+	if ( '1' == $filter_value ) {
+		$wpquery->set( 'meta_query', array(
+			array(
+				'key'     => '_thumbnail_id',
+				'compare' => 'EXISTS'
+			)
+		) );
+	} else if ( '2' == $filter_value ) {
+		$wpquery->set( 'meta_query', array(
+			array(
+				'key'     => '_thumbnail_id',
+				'compare' => 'NOT EXISTS'
+			)
+		) );
+	}
+
+
+}
+
+add_action( 'pre_get_posts', 'coldemo_wordcount_filter_data' );
+// End WordCount Filter 
+
 
